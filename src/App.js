ac0d1fff1cef67589dd644ead8f4da0db3fab113
yejin <yejin213527@gmail.com>
2023-05-24 14:09:18 +0900

    0524 실습 - 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import { Component } from "react";
-import CSSModule from "./CSSModule";
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <CSSModule />
-      </div>
-    );
-  }
-}
+import styled from "styled-components";
+const SimpleButton = styled.button`
+  color: white;
+  background-color: green;
+`;
+const LargeButton = styled(SimpleButton)`
+  font-size: 50px;
+`;
+// 일반적인 방법
+const ReactButton = (props) => {
+  return <button>{props.children}</button>;
+};
+const ReactLargeButton = styled(ReactButton)`
+  font-size: 50px;
+`;
+const App = () => {
+  return (
+    <div>
+      <SimpleButton>Simple</SimpleButton>
+      {/* 렌더링이 될 경우에 클래스 이름을 자동적으로 붙여준다 */}
+      <LargeButton>Large</LargeButton>
+      <ReactButton>React</ReactButton>
+      <ReactLargeButton>React Large</ReactLargeButton>
+    </div>
+  );
+};
 export default App;
--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import styles from './CSSModule.module.css';
+import React from "react";
+import styles from "./CSSModule.module.css";
 const CSSModule = () => {
-return (
-<div className={styles.wrapper}>
-안녕하세요, 저는 <span className="something">CSS Module!</span>
-</div>
-);
+  return (
+    <div className={`${styles.wrapper} ${styles.inverted}`}>
+      {/* 렌더링을 하게 되면 클래스 이름을 CSSModule_wrapper_hqEWL(해시값)으로 변경
+    CSSModule_inverted_bfBSQ(해시값)
+    해시값은 클래스 이름을 구별하기 위해서 자동으로 붙여준다 */}
+      안녕하세요, 저는 <span className="something">CSS Module!</span>
+    </div>
+  );
 };
 export default CSSModule;
